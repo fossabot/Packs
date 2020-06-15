174e77f5002e013108eb85d9828e22cea69b393f
dayknightmare <miguelhunter95@gmail.com>
2020-06-15 11:23:51 -0300

    Melhoria na instalação de pacotes

--- devpip/makepip.py
+++ devpip/makepip.py
@@ -4,9 +4,93 @@ import sys
 import os
 
 
-def makepip():
-    if hasattr(sys, 'real_prefix'):
+class Makepip():
+    def __init__(self, ar:list, commands:dict):
+        self.__sysargs = ar
+        self.__commands = commands
 
+        if self.__sysargs[1].lower() == 'install' or self.__sysargs[1].lower() == 'i':
+            self.__install()
+
+        elif self.__sysargs[1].lower() == 'uninstall' or self.__sysargs[1].lower() == 'rm' or self.__sysargs[1].lower() == 'remove':
+            self.__remove()
+
+    
+    def __install(self):
+        procss = ['pip', 'install']
+
+        for i in self.__sysargs[2:]:
+            p = list(procss)
+            p.append(i)
+
+            print(f"\033[93mInstalling {i}\033[37m", end='\r')
+
+            r = subprocess.run(p, stdout=subprocess.PIPE)
+
+            if f'Requirement already satisfied: {i}' in r.stdout.decode():
+                print(f"\033[92m{i} is already installed                         \n\033[37m")
+
+            else:
+                x = r.stdout.decode().split('Successfully installed ')[1].split(' ')[0].split('-')[1]
+                print(f"\033[92m{i} installed in version {x}        \n\033[37m")
+
+            try:
+                r = r.stdout.decode().split("Successfully installed ")
+                r = r[1].replace('\\r\\n', '').split(' ')
+
+            except IndexError:
+                continue
+
+            print('Dependencies')
+
+            for p in r:
+                packageVersion = p.split('-')
+                packageVersion = packageVersion[len(packageVersion) - 1]
+            
+                p = p.replace('-' + packageVersion, f'=={packageVersion}')
+                
+                if not p.lower().startswith(i.lower()):
+                    print(p)
+            
+                with open('requirements.txt', 'a') as f:
+                    f.write(p + '\r')
+
+
+    def __remove(self):
+        lines = []
+        delPackages = []
+
+        for i in self.__sysargs[2:]:
+            while True:
+                opt = input(f"\nDo you want remove {i} [y, n]\n>>> ")
+
+                if opt.lower() == 'y' or opt.lower() == 'n':
+                    if opt.lower() == 'y':
+                        delPackages.append(i)
+                        os.system(f'pip {commands[self.__sysargs[1].lower()]} {i} -y')
+                    break
+                
+                else:
+                    print('\n\033[91mType a valid option\033[37m\n')
+
+        with open('requirements.txt', 'r') as f:
+            lines = f.readlines()
+
+        with open('requirements.txt', 'w') as f:
+            for i in lines:
+                b = True
+
+                for j in delPackages:
+                    if i.lower().split('==')[0] == j or i == '\r' or i == '\n':
+                        b = False
+                        break
+                    
+                if b:
+                    f.write(i)
+
+
+if __name__ == '__main__':
+    if hasattr(sys, 'real_prefix'):
         commands = {
             'install': 'install', 
             'i': 'install', 
@@ -23,67 +107,12 @@ def makepip():
             print('\033[92m uninstall <package>\033[37m  to uninstall a package')
             print('\033[92m remove    <package>\033[37m  to uninstall a package')
             print('\033[92m rm        <package>\033[37m  to uninstall a package\n')
-            
-            return False
 
         elif sys.argv[1].lower() not in commands:
             print('\033[91m Invalid command\033[37m')
-            return False
-
-        if sys.argv[1].lower() == 'install' or sys.argv[1].lower() == 'i':
-            r = subprocess.run(['pip', commands[sys.argv[1].lower()], *sys.argv[2:]], stdout=subprocess.PIPE)
-
-            print(r.stdout.decode())
-
-            r = r.stdout.decode().split("Successfully installed ")
-            r = r[1].replace('\\r\\n', '').split(' ')
-
-            for i in r:
-                packageVersion = i.split('-')
-                packageVersion = packageVersion[len(packageVersion) - 1]
-            
-                i = i.replace('-' + packageVersion, f'=={packageVersion}')
-                print(i)
-            
-                with open('requirements.txt', 'a') as f:
-                    f.write(i + '\r')
 
         else:
-            lines = []
-            delPackages = []
+            Makepip(sys.argv, commands)
 
-            for i in sys.argv[2:]:
-                while True:
-                    opt = input(f"Do you want remove {i} [y,n]\n>>> ")
-
-                    if opt.lower() == 'y' or opt.lower() == 'n':
-                        if opt.lower() == 'y':
-                            delPackages.append(i)
-                            os.system(f'pip {commands[sys.argv[1].lower()]} {i} -y')
-                        break
-                    
-                    else:
-                        print('\n\033[91mType a option\033[37m\n')
-
-            with open('requirements.txt', 'r') as f:
-                lines = f.readlines()
-
-            with open('requirements.txt', 'w') as f:
-                for i in lines:
-                    b = True
-
-                    for j in delPackages:
-                        if i.lower().split('==')[0] == j:
-                            b = False
-                            break
-
-                    if b:
-                        f.write(i)
     else:
         print("Please activate the virtual environment to use DevPip")
-
-
-if __name__ == '__main__':
-    # r = subprocess.run(['pip', 'freeze'], stdout=subprocess.PIPE)
-    # print(r.stdout)
-    makepip()
